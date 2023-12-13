97c6d50d6e7899203bda14b0946719437a5c210c
Micael Oliveira <micael.oliveira@anu.edu.au>
2023-12-13 15:12:43 +1100

    Add project boiler-plate.

---
+++ om3utils/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for om3utils."""
+
+__author__ = """Micael Oliveira"""
+__email__ = "micael.oliveira@anu.edu.au"
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
